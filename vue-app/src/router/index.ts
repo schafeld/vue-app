56c46701c441c18a235774a381f6680824bbab6d
Oliver Schafeld <oliver@schafeld.com>
2025-04-14 14:56:50 +0200

    display route parameter

--- vue-app/src/router/index.ts
+++ vue-app/src/router/index.ts
@@ -13,6 +13,11 @@ const router = createRouter({
       path: '/projects',
       name: 'projects',
       component: () => import('../views/ProjectsView.vue')
+    },
+    {
+      path: '/projects/:id',
+      name: 'project',
+      component: () => import('@/views/SingleProjectView.vue'),
     }
   ]
 })
---
+++ vue-app/src/views/SingleProjectView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="single-project-view">
+    <h1>Single Project View</h1>
+    <RouterLink to="/projects">Back to Projects</RouterLink>
+    <div class="project-details">
+      <h2>Project Details</h2>
+      <!-- Add project details here -->
+      <p>Project ID: {{ route.params?.id }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { useRoute } from 'vue-router';
+  const route = useRoute();
+</script>
+
+<style>
+  .single-project-view {
+    text-align: center;
+  }
+</style>
