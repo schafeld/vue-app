ec6f960afa97d54dc768a6bcdd9ec5edab2177fe
Oliver Schafeld <oliver@schafeld.com>
2025-05-21 11:20:32 +0200

    add dropdown shadcdn component

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -3,6 +3,14 @@ import { supabase } from '@/lib/supabaseClient';
 import { Button } from '@/components/ui/button'
 import { useRoute } from 'vue-router';
 import Input from './components/ui/input/Input.vue';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 const route = useRoute();
 console.log( supabase);
@@ -21,7 +29,20 @@ console.log( supabase);
       <form class="w-full max-w-96">
       <Input class="w-full pl-8 bg-background" type="text" placeholder="Search ..." />
     </form>
-    <div class="w-8 h-8 rounded-full bg-white"></div>
+
+    <DropdownMenu>
+    <DropdownMenuTrigger>
+      <div class="avatar w-8 h-8 rounded-full bg-white"></div>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <DropdownMenuLabel>My Account</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem>Profile</DropdownMenuItem>
+      <DropdownMenuItem>Billing</DropdownMenuItem>
+      <DropdownMenuItem>Team</DropdownMenuItem>
+      <DropdownMenuItem>Subscription</DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
     </nav>
     <RouterView />
     <div v-if="route.path !== '/'" class="mt-5 flex flex-col items-center">
