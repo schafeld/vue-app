fff86aeba8fa4c1246a0cc98b0cca22fd20b1a8a
Oliver Schafeld <oliver@schafeld.com>
2025-05-21 16:00:43 +0200

    move layout parts into dedicated components

---
+++ vue-app/src/components/layout/TopNavbar.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import Input from '@/components/ui/input/Input.vue'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+</script>
+
+<template>
+  <nav class="h-16 border-b bg-muted/40 flex gap-2 justify-between px-6 items-center">
+    <form class="relative h-fit w-full max-w-96">
+      <iconify-icon
+        class="absolute top-[50%] translate-y-[-50%] left-2.5 text-muted-foreground"
+        icon="lucide:search"
+      ></iconify-icon>
+      <Input class="w-full pl-8 bg-background" type="text" placeholder="Search ..." />
+    </form>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar class="border border-gray-500">
+          <AvatarImage src="https://avatars.githubusercontent.com/u/5053821?v=4" alt="Olli"/>
+          <AvatarFallback>OS</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
