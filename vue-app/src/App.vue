e58836c640a0f03e1a1724091972c6145f9918d6
Oliver Schafeld <oliver@schafeld.com>
2025-05-23 16:14:29 +0200

    use separate layout template component

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,26 +1,11 @@
 <script setup lang="ts">
-  import { useRoute } from 'vue-router';
-  import { Button } from '@/components/ui/button'
-  import TopNavbar from '@/components/layout/TopNavbar.vue'
-  import Sidebar from './components/layout/Sidebar.vue';
-
-  const route = useRoute();
+  import AuthLayout from './components/Layout/main/AuthLayout.vue';
 </script>
 
 <template>
-  <Sidebar />
-  <div class="content-area flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <TopNavbar />
-    <main>
-      <RouterView />
-    </main>
-
-    <div v-if="route.path !== '/'" class="my-5 flex flex-col items-center">
-      <RouterLink to="/">
-      <Button>Back to home</Button>
-      </RouterLink>
-    </div>
-  </div>
+  <AuthLayout>
+    <RouterView />
+  </AuthLayout>
 </template>
 
 
---
+++ vue-app/src/components/layout/main/AuthLayout.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import { useRoute } from 'vue-router';
+  import { Button } from '@/components/ui/button'
+  import TopNavbar from '@/components/Layout/TopNavbar.vue'
+  import Sidebar from '@/components/Layout/Sidebar.vue';
+
+  const route = useRoute();
+</script>
+
+<template>
+  <Sidebar />
+  <div class="content-area flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNavbar />
+    <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+      <div class="flex items-center">
+        <h1 class="text-lg font-semibold md:text-2xl">
+          <RouterLink :to="route.path" class="text-muted-foreground">
+            {{ route.meta.title }}
+          </RouterLink>
+        </h1>
+        </div>
+      <slot />
+    </main>
+
+    <div v-if="route.path !== '/'" class="my-5 flex flex-col items-center">
+      <RouterLink to="/">
+      <Button>Back to home</Button>
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+
+
