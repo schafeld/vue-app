84f2ea0f92f9b14d2e741ebe554d9dc53ee6b2dc
Oliver Schafeld <oliver@schafeld.com>
2025-05-29 10:17:22 +0200

    if user got to single project view from tasks page, make 'back' go to previous page otherwise back to projects overview

--- vue-app/src/pages/projects/[slug].vue
+++ vue-app/src/pages/projects/[slug].vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
   const route = useRoute();
+  const router = useRouter();
   const params = route.params as { slug: string };
 
   usePageStore().pageData.title = 'Single Project View';
+
+  const goBack = () => {
+    if (router.options.history.state.back) {
+      router.back();
+    } else {
+      router.push('/projects');
+    }
+  };
 </script>
 
 <template>
   <div class="single-project-view">
-    <RouterLink to="/projects">Back to Projects</RouterLink>
+    <!-- sub navigation inline link list -->
+    <div class="sub-nav inline-flex items-center gap-4">
+      <!-- <RouterLink to="/projects" class="hover:underline">Projects Overview</RouterLink> -->
+      <button @click="goBack" class="hover:underline cursor-pointer">Go Back</button>
+    </div>
+
     <div class="project-details bg-gray-900 p-6 rounded-lg shadow-sm my-4">
       <h2 class="xl my-2">Project Details</h2>
       <!-- Add project details here -->
