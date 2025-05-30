cd15ec5b27e21a7a976797c1cbb7bd7e40b15fec
Oliver Schafeld <oliver@schafeld.com>
2025-05-30 14:57:38 +0200

    display single task details

--- vue-app/src/pages/tasks/[id].vue
+++ vue-app/src/pages/tasks/[id].vue
@@ -1,20 +1,118 @@
 <script setup lang="ts">
-  const route = useRoute();
-  const params = route.params as { id: string };
+import { singleTaskQuery } from "@/utils/supabaseQueries";
+import type { SingleTask } from "@/utils/supabaseQueries";
 
-  usePageStore().pageData.title = 'Single Task View';
+const route = useRoute("/tasks/[id]");
+// const params = route.params as { id: string };
+
+const task = ref<SingleTask | null>(null);
+
+watch(
+  task,
+  (newTask) => {
+    if (newTask && newTask.name) {
+      usePageStore().pageData.title = `Task: ${newTask.name}`;
+    } else {
+      usePageStore().pageData.title = "Task: Loading...";
+    }
+  },
+  { immediate: true }
+);
+
+const getTask = async () => {
+  const { data, error } = await singleTaskQuery(Number(route.params.id));
+
+  if (error) {
+    console.error("Error fetching task:", error);
+  } else {
+    task.value = data;
+  }
+};
+
+await getTask();
 </script>
 
 <template>
-  <div class="single-project-view">
-    <RouterLink to="/tasks">Back to Tasks</RouterLink>
-    <div class="project-details bg-gray-900 p-6 rounded-lg shadow-sm my-4">
-      <h2 class="xl my-2">Project Details</h2>
-      <!-- Add project details here -->
-      <p>Task: {{ params.id }}</p>
-    </div>
-  </div>
+  <RouterLink to="/tasks">Back to Tasks</RouterLink>
+  <Table v-if="task" class="w-full">
+    <TableRow>
+      <TableHead> Name </TableHead>
+      <TableCell> {{ task.name }} </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead> Description </TableHead>
+      <TableCell>
+        {{ task.description }}
+      </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead> Assignee </TableHead>
+      <TableCell> Todo </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead> Project </TableHead>
+      <TableCell>
+        <RouterLink
+          v-if="task.projects?.slug"
+          :to="`/projects/${task.projects.slug}`"
+          class="hover:underline"
+        >
+          {{ task.projects?.name }}
+        </RouterLink>
+        <span v-else>{{ task.projects?.name }}</span>
+      </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead> Status </TableHead>
+      <TableCell> {{ task.status }}</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead> Collaborators </TableHead>
+      <TableCell>
+        <div class="flex">
+          <Avatar
+            class="-mr-4 border border-primary hover:scale-110 transition-transform"
+            v-for="collaborator in task.collaborators"
+            :key="collaborator"
+          >
+            <RouterLink class="w-full h-full flex items-center justify-center" to="">
+              <AvatarImage src="" alt="" />
+              <AvatarFallback> </AvatarFallback>
+            </RouterLink>
+          </Avatar>
+        </div>
+      </TableCell>
+    </TableRow>
+    <TableRow class="hover:bg-transparent">
+      <TableHead class="align-top pt-4"> Comments </TableHead>
+
+      <TableCell>
+        Comments cards goes in here..
+
+        <div class="flex flex-col justify-between p-3 bg-muted my-2 rounded-md">
+          <textarea
+            placeholder="Add your comment.."
+            class="w-full max-w-full overflow-y-auto prose-sm prose border rounded dark:prose-invert hover:border-muted bg-background border-muted p-3"
+          >
+          </textarea>
+          <div class="flex justify-between mt-3">
+            <Button> Comment </Button>
+            <div class="flex gap-4">
+              <button variant="ghost" @click.prevent>
+                <iconify-icon icon="lucide:paperclip"></iconify-icon>
+                <span class="sr-only">Attach file</span>
+              </button>
+              <button variant="ghost" @click.prevent>
+                <iconify-icon icon="lucide:image-up"></iconify-icon>
+
+                <span class="sr-only">Upload image</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </TableCell>
+    </TableRow>
+  </Table>
 </template>
 
-<style>
-</style>
+<style></style>
--- vue-app/src/utils/supabaseQueries.ts
+++ vue-app/src/utils/supabaseQueries.ts
@@ -38,3 +38,18 @@ export const singleProjectQuery = (slug: string) => supabase
     .single()
 
 export type SingleProject = QueryData<ReturnType<typeof singleProjectQuery>>;
+
+export const singleTaskQuery = (id: number) => supabase
+  .from('tasks')
+  .select(`
+    *,
+    projects (
+      id,
+      name,
+      slug
+    )
+  `)
+  .eq('id', id)
+  .single();
+
+export type SingleTask = QueryData<ReturnType<typeof singleTaskQuery>>;
