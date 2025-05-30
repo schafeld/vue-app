883acc6ea85ea2238454c6a8c2761bcfe5d79e9d
Oliver Schafeld <oliver@schafeld.com>
2025-05-30 14:01:40 +0200

    display task details belonging to project

--- vue-app/src/pages/projects/[slug].vue
+++ vue-app/src/pages/projects/[slug].vue
@@ -42,20 +42,16 @@ await getProject();
 </script>
 
 <template>
-  <Table>
+  <Table v-if="project" class="w-full">
     <button @click="goBack" class="hover:underline cursor-pointer">Go Back</button>
     <TableRow>
       <TableHead> Name </TableHead>
-      <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+      <TableCell> {{ project.name }} </TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Description </TableHead>
       <TableCell>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad iure qui tempora ex
-        nihil, ab reprehenderit dolorem sunt veritatis perferendis? Repudiandae quis velit
-        quasi ab natus quia ratione voluptas deserunt labore sed distinctio nam fuga fugit
-        vero voluptates placeat aperiam, saepe excepturi eos harum consectetur doloremque
-        perspiciatis nesciunt! Incidunt, modi.
+        {{ project.description || "No description provided." }}
       </TableCell>
     </TableRow>
     <TableRow>
@@ -68,8 +64,8 @@ await getProject();
         <div class="flex">
           <Avatar
             class="-mr-4 border border-primary hover:scale-110 transition-transform"
-            v-for="n in 5"
-            :key="n"
+            v-for="collaborator in project.collaborators"
+            :key="collaborator"
           >
             <RouterLink class="w-full h-full flex items-center justify-center" to="">
               <AvatarImage src="" alt="" />
@@ -81,7 +77,10 @@ await getProject();
     </TableRow>
   </Table>
 
-  <section class="mt-10 flex flex-col md:flex-row gap-5 justify-between grow">
+  <section
+    v-if="project"
+    class="mt-10 flex flex-col md:flex-row gap-5 justify-between grow"
+  >
     <div class="flex-1">
       <h2>Tasks</h2>
       <div class="table-container">
@@ -94,10 +93,10 @@ await getProject();
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow v-for="n in 5" :key="n">
-              <TableCell> Lorem ipsum dolor sit amet. </TableCell>
-              <TableCell> In progress </TableCell>
-              <TableCell> 22/08/2024 </TableCell>
+            <TableRow v-for="task in project.tasks" :key="task.id">
+              <TableCell> {{ task.name }} </TableCell>
+              <TableCell> {{ task.status }} </TableCell>
+              <TableCell> {{ task.due_date }} </TableCell>
             </TableRow>
           </TableBody>
         </Table>
