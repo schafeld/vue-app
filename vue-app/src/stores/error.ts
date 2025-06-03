672e8ffd30bad7b1806b483829025668a958cbe6
Oliver Schafeld <oliver@schafeld.com>
2025-06-03 13:07:42 +0200

    make error code number optional

--- vue-app/src/stores/error.ts
+++ vue-app/src/stores/error.ts
@@ -4,7 +4,7 @@ import type { PostgrestError } from '@supabase/supabase-js';
 export const useErrorStore = defineStore('error-store', () => {
   const activeError = ref<null | CustomError>(null);
 
-  const setError = ({ error, customCode }: {error:string | PostgrestError, customCode: number}) => {
+  const setError = ({ error, customCode }: {error:string | PostgrestError | Error , customCode?: number}) => {
     const message = typeof error === 'string' ? error : error.message;
     activeError.value = Error(message);
     activeError.value.customCode = customCode;
