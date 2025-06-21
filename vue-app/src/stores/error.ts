7e848118699c5375b3a5c54658d3da10af13dd62
Oliver Schafeld <oliver@schafeld.com>
2025-06-21 14:31:15 +0200

    enable store hot module replacement

--- vue-app/src/stores/error.ts
+++ vue-app/src/stores/error.ts
@@ -28,3 +28,7 @@ export const useErrorStore = defineStore('error-store', () => {
     isCustomError
   }
 });
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useErrorStore, import.meta.hot))
+}
