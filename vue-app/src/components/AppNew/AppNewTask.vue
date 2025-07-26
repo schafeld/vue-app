f996cc5adcf2514bc7f66772711f38d80a1ce775
Oliver Schafeld <oliver@schafeld.com>
2025-07-26 14:31:15 +0200

    add create new task form elements

--- vue-app/src/components/AppNew/AppNewTask.vue
+++ vue-app/src/components/AppNew/AppNewTask.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
+import type { CreateNewTask } from '@/types/CreateNewForm';
 const sheetOpen = defineModel<boolean>();
+
+const createTask = async (formData: CreateNewTask) => {
+  // Logic to create a new task
+  console.log('Creating new task...');
+
+  await new Promise((resolve) => setTimeout(
+    () => {
+      resolve(console.log('Task created successfully!', formData));
+    }, 2000)); // Simulate API call
+
+  // sheetOpen.value = false; // Close the sheet after creating the task
+};
 </script>
 
 <template>
@@ -11,7 +24,33 @@ const sheetOpen = defineModel<boolean>();
 
         </SheetHeader>
             <div class="ml-4" >
-                <FormKit type="text" name="taskName" label="Task Name" placeholder="Enter task name" />
+              <FormKit type="form" @submit="createTask" submit-label="Create Task">
+                <FormKit type="text" name="taskName" id="taskName" label="Task Name" placeholder="Enter new task name" />
+                <FormKit
+                  type="select"
+                  name="profile_id"
+                  id="profile_id"
+                  label="User"
+                  placeholder="Select a user"
+                  :options="[{ label: 'Select a user', value: 1 }]"
+                />
+                <FormKit
+                  type="select"
+                  name="project_id"
+                  id="project_id"
+                  label="Project"
+                  placeholder="Select a project"
+                  :options="[{ label: 'Select a project', value: 1 }]"
+                />
+                <FormKit
+                  type="textarea"
+                  name="description"
+                  id="description"
+                  label="Description"
+                  placeholder="Task description"
+                  validation="length:0,500"
+                />
+              </FormKit>
             </div>
       </SheetContent>
     </Sheet>
---
+++ vue-app/src/types/CreateNewForm.ts
@@ -0,0 +1,6 @@
+export interface CreateNewTask {
+  taskName: string;
+  description: string;
+  project_id: number;
+  profile_id: string;
+}
