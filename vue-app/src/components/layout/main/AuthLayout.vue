b02dfae172555780c1e6d51f734e49e91db77769
Oliver Schafeld <oliver@schafeld.com>
2025-08-04 16:26:40 +0200

    add fade transitions to route and layout changes

--- vue-app/src/components/layout/main/AuthLayout.vue
+++ vue-app/src/components/layout/main/AuthLayout.vue
@@ -20,27 +20,27 @@
 </script>
 
 <template>
-  <Sidebar @open-new-task="taskSheetOpen = true" />
-  <AppNewTask v-model="taskSheetOpen" />
-
-  <div 
-    class="content-area flex flex-col transition-[margin]"
-    :class="{ 'ml-52': isMenuOpen, 'ml-24': !isMenuOpen }"
-  >
-    <TopNavbar />
-    <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <div class="flex items-center">
-        <h1 class="text-lg font-semibold md:text-2xl">
-          {{ pageData.title }}
-        </h1>
-        </div>
-      <slot />
-    </main>
-
-    <div v-if="route.path !== '/'" class="my-5 flex flex-col items-center">
-      <RouterLink to="/">
-      <Button>Back to home</Button>
-      </RouterLink>
+  <div class="transition-wrapper">
+    <Sidebar @open-new-task="taskSheetOpen = true" />
+    <AppNewTask v-model="taskSheetOpen" />
+    <div
+      class="content-area flex flex-col transition-[margin]"
+      :class="{ 'ml-52': isMenuOpen, 'ml-24': !isMenuOpen }"
+    >
+      <TopNavbar />
+      <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <div class="flex items-center">
+          <h1 class="text-lg font-semibold md:text-2xl">
+            {{ pageData.title }}
+          </h1>
+          </div>
+        <slot />
+      </main>
+      <div v-if="route.path !== '/'" class="my-5 flex flex-col items-center">
+        <RouterLink to="/">
+        <Button>Back to home</Button>
+        </RouterLink>
+      </div>
     </div>
   </div>
 </template>
